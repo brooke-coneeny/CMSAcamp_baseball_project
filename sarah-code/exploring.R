8919cf3bf97c55d02c92aa1615cde656072c296b
Sarah Sult <ssult@wustl.edu>
2021-06-27 12:51:48 -0500

    need something better than stat_smooth

--- sarah-code/exploring.R
+++ sarah-code/exploring.R
@@ -87,49 +87,21 @@ batter_all_2021 <- bind_rows(april, may2021, june_2021)
 #need to find each hitters max EV and the corresponding launch angle and their wOBA for the season and 
 #each data point is a person
 
-# batter_all_2021 %>%
-#   filter(description == "hit_into_play", !is.na(events), launch_angle %in% c(-40:60))%>%
-#   mutate(events_group = case_when(
-#     events %in% c("field_out", "other_out", "grounded_into_double_play", "double_play", 
-#                   "fielders_choice_out", "force_out", "sac_bunt", "sac_fly_double_play", 
-#                   "sac_bunt_double_play", "field_error", "sac_fly", "fielders_choice", "triple_play") ~ "out",
-#     TRUE ~ "hit"
-#   ), wOBA = case_when(
-#     events == "single" ~ .882,
-#     events == "double" ~ 1.242,
-#     events == "triple" ~ 1.586,
-#     events == "home_run" ~ 2.039,
-#     events_group == "out" ~ 0
-#   ), launch_speed_group = case_when(
-#     launch_speed < 106 ~ "low",
-#     launch_speed >= 106 & launch_speed < 112 ~ "medium",
-#     launch_speed >= 112 ~ "high"
-#   )
-#   )%>%
-#   dplyr::select(wOBA, launch_speed, launch_angle, launch_speed_group)%>%
-#   ggplot(aes(x=launch_angle, y=wOBA, color = launch_speed_group))+
-#   geom_smooth(method="loess", se=FALSE)+
-#   theme_bw()
-
-#gives us totals for all variables but singles, could calculate singles though???????????
-#how do you merge players on different teams? the playerID is confusing???????????
-batting_Lahman <- Lahman::Batting
-batting_Lahman_2019 <- batting_Lahman %>%     #doens't have 2021 data yet! also doesnt count singles?!?!
-  filter(yearID == 2019)
-
 wOBA_angle_velo_graph <- batter_all_2019 %>%
 #this gets us each player's max EV and the associated angle
   group_by(player_name) %>%
   summarize(max_EV = max(launch_speed, na.rm = TRUE)) %>%
   filter(max_EV < 130 & max_EV>0) %>%
-  left_join(batter_all_2021, by = c("player_name", "max_EV" = "launch_speed")) %>%
+  left_join(batter_all_2019, by = c("player_name", "max_EV" = "launch_speed")) %>%
 #this brings in expected stats data
   left_join(expected_stats_2019, by = c("player_name" = "name")) %>%
   select(player_name, max_EV, launch_angle, woba) %>%
+  filter(!is.na(woba)) %>%
 #then we prepare the graph
   mutate(EV_group = cut(max_EV, breaks = c(0, 105.99, 111.99, 150), labels = c("<106", "106+", "112+"))) %>%
   ggplot(aes(x=launch_angle, y = woba, color = EV_group)) +
   geom_smooth(se = FALSE)+
+  #geom_line()+
   scale_x_continuous(limits = c(-40, 60))+
   scale_y_continuous(limits = c(0, 0.8))+
   theme_bw()+
