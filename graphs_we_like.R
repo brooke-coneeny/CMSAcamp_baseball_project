63c33dc681ca2a678f441434a68215e878b12784
Sarah Sult <ssult@wustl.edu>
2021-06-29 14:55:06 -0500

    twitter graph works, started folder to compile good graphs

--- graphs_we_like.R
+++ graphs_we_like.R
@@ -1,11 +1,7 @@
-library(baseballr)
-library(tidyverse)
-
-#baseballr package info: http://billpetti.github.io/baseballr/
-#variable meanings: https://app.box.com/v/statcast-pitchfx-glossary-pett
+#can put graphs that we want to present in here
 
 
-# Load data ---------------------------------------------------------------
+# Loading Data ------------------------------------------------------------
 
 batter_all_2019 <- read_rds("data/all2019data.rds")
 
@@ -17,7 +13,7 @@ in_play_2019 <- batter_all_2019 %>%
   select(launch_angle, woba_value, player_name, launch_speed) 
 
 #max EV per player and combine with balls in play (so there is a column categorizing the player who 
-  #hit the ball by their max EV in the season)
+#hit the ball by their max EV in the season)
 max_EV_2019_grouped2 <- batter_all_2019 %>%
   group_by(player_name) %>%
   summarize(max_EV = max(launch_speed, na.rm = TRUE)) %>%
@@ -62,45 +58,4 @@ wOBA_angle_velo_graph_3 <- max_EV_2019_grouped3 %>%
   labs(x= "Launch Angle",
        y = "wOBA",
        title = "wOBA by Launch Angle")
-  
-  
 
-# Do righties vs. lefties have a distinct ev/angle combo?  --------------------------------------------
-
-batter_all_2019 %>%
-  group_by(player_name) %>%
-  summarize(max_EV = max(launch_speed, na.rm = TRUE)) %>%
-  filter(max_EV < 130 & max_EV>0) %>%
-  left_join(batter_all_2019, by = c("player_name", "max_EV" = "launch_speed")) %>%
-  select(player_name, max_EV, launch_angle, stand)%>%
-  ggplot(aes(x=launch_angle, y=max_EV, color = stand))+
-  geom_point(alpha = .4)+
-  theme_bw()+
-  labs(x = "Launch Angle",
-       y = "Max Exit Velocity")
-
-
-# where the max ev hits landed -----------------------------------------------------------------------
-
-batter_all_2019 %>%
-  group_by(player_name) %>%
-  summarize(max_EV = max(launch_speed, na.rm = TRUE)) %>%
-  filter(max_EV < 130 & max_EV>0) %>%
-  left_join(batter_all_2019, by = c("player_name", "max_EV" = "launch_speed"))%>%
-  select(player_name, hc_x, hc_y, stand, events)%>%
-  filter(!is.na(stand), !is.na(events))%>%
-  mutate(events = case_when(
-#for the purposes of this, field error and fielders choice are outs because in normal play that's what 
-    #they would have been
-    events %in% c("double_play", "field_out", "grounded_into_double_play", "sac_bunt", 
-                  "sac_fly", "field_error", "fielders_choice") ~ "out",
-    events %in% c("single", "force_out") ~ "single",
-    events == "double" ~ "double",
-    events == "home_run" ~ "home_run",
-    events == "triple" ~ "triple"
-  ))%>%
-#coloring by event might allow us to see how the shift affects lefties
-  ggplot(mapping = aes(x = hc_x, y = hc_y, color = events)) +
-  geom_point() +
-  facet_wrap(~stand) +
-  scale_y_reverse()
