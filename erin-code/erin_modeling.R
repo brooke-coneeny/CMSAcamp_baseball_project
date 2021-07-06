a5dc6cb504052c4b7fce836a011fe48f6b00d698
Erin Franke <efranke@macalester.edu>
2021-07-06 14:57:45 -0500

    committing additional work on gam model and writing 2020 data

--- erin-code/erin_modeling.R
+++ erin-code/erin_modeling.R
@@ -3,6 +3,7 @@ library(glmnet)
 library(mgcv)
 
 batter_all_2019 <- read_rds("private_data/all2019data.rds")
+batter_all_2021 <- read_rds("private_data/all2021data.rds")
 
 woba_model <- gam(woba_value ~ s(launch_angle) + s(launch_speed), 
                   data = batter_all_2019, method = "REML")
@@ -12,24 +13,41 @@ woba_model <- gam(woba_value ~ s(launch_angle) + s(launch_speed),
 summary(woba_model)
 
 #cannot draw horizontal line through either plot indicating significance. 
+#histogram looks okay but too tall in middle, qq plot looks decent
+#residual plots look kind of frightening...due to some variable not sure why yet
 plot(woba_model, pages =1, all.terms = TRUE, shade = TRUE, shade.col = "lightblue")
 
-#check model adequacy - the pvalue for launch speed especially is relatively small
-#so we may want to try a different value for k
+#check model adequacy - the pvalues are not too low which is good
 gam.check(woba_model, pages = 1)
 
+#check concurvity... looks good because there are no high values 
+concurvity(woba_model, full = TRUE)
+
+#plotting launch angle and exit velocity versus woba, the plots look 
+#very similar to the output of the plot gam command
+batter_all_2019 %>%
+  ggplot(aes(x=launch_angle, y=woba_value))+
+  geom_smooth()
+
+batter_all_2019 %>%
+  ggplot(aes(x=launch_speed, y=woba_value))+
+  geom_smooth()
+
+#trying out the model with a few players 
+mike_trout <- batter_all_2021 %>%
+  filter(player_name == "Trout, Mike", description == "hit_into_play") 
+jason_heyward <- batter_all_2021 %>%
+  filter(player_name == "Heyward, Jason", description == "hit_into_play") 
+
+#this is about 0.1 above what we would expect 
+tibbletest <- tibble(gam.preds = predict(woba_model, newdata = jason_heyward))
+mean(tibbletest$gam.preds, na.rm = TRUE)
+
+
+
+
+#worse model
 woba_model2 <- gam(woba_value ~ s(launch_angle, k =10) + s(launch_speed, k=15), 
                    data = batter_all_2019, method = "REML", sp = 0.01)
 
-#both edf values are high meaning wiggly relationship with woba, significant p-values
-summary(woba_model2) 
 
-#histogram looks okay but too tall in middle, qq plot looks decent
-#residual plots look kind of frightening...due to some variable not sure why yet
-plot(woba_model2, pages =1, all.terms = TRUE, shade = TRUE, shade.col = "lightblue")
-
-#pvalues are not too low which is good
-gam.check(woba_model2)
-
-#check concurvity... looks good because there are no high values 
-concurvity(woba_model2, full = TRUE)
---
+++ erin-code/erin_writingdata.R
@@ -0,0 +1,22 @@
+july20 <- scrape_statcast_savant_batter_all(start_date = "2020-07-23",
+                                            end_date = "2020-07-31")
+aug201 <- scrape_statcast_savant_batter_all(start_date = "2020-08-01",
+                                            end_date = "2020-08-07")
+aug202 <- scrape_statcast_savant_batter_all(start_date = "2020-08-08",
+                                            end_date = "2020-08-14")
+aug203 <- scrape_statcast_savant_batter_all(start_date = "2020-08-15",
+                                            end_date = "2020-08-22")
+aug204 <- scrape_statcast_savant_batter_all(start_date = "2020-08-23",
+                                            end_date = "2020-08-31")
+sep201 <- scrape_statcast_savant_batter_all(start_date = "2020-09-01",
+                                            end_date = "2020-09-07")
+sep202 <- scrape_statcast_savant_batter_all(start_date = "2020-09-08",
+                                            end_date = "2020-09-14")
+sep203 <- scrape_statcast_savant_batter_all(start_date = "2020-09-15",
+                                            end_date = "2020-09-23")
+sep204 <- scrape_statcast_savant_batter_all(start_date = "2020-09-24",
+                                            end_date = "2020-09-30")
+batter_all_2020 <- rbind(july20, aug201, aug202, aug203, aug204, sep201, sep202, 
+                         sep203, sep204)
+
+write_rds(batter_all_2020, file ="private_data/all2020data.rds")
