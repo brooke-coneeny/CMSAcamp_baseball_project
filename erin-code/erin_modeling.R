bd1f342b20439b49a074608c549e733ebffff5db
Erin Franke <efranke@macalester.edu>
2021-07-08 12:41:49 -0500

    tried some stuff out with a logistic model for homerun probability

--- erin-code/erin_modeling.R
+++ erin-code/erin_modeling.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(glmnet)
 library(mgcv)
 library(data.table)
+library(caret)
 
 batter_all_2019 <- read_rds("private_data/all2019data.rds")
 batter_all_2021 <- read_rds("private_data/all2021data.rds")
@@ -52,15 +53,19 @@ mean(tibbletest$gam.preds, na.rm = TRUE)
 # modeling..interactions?-------------------------------------------------
 
 #create model training data
-set.seed(7821)
 batter_all_1921hp <- data.table(batter_all_1921hp)
-training_data <- batter_all_1921hp[sample(.N, 112042)]
+
+set.seed(112)
+train_i <- createDataPartition(y = batter_all_1921hp$woba_value, p = 0.7, list = FALSE)
+train_mlb_data <- batter_all_1921hp[train_i,]
+test_mlb_data <- batter_all_1921hp[-train_i,]
 
 woba_model2 <- gam(woba_value ~ s(launch_angle, launch_speed), data = training_data, method = "REML")
 
 summary(woba_model2)
 coef(woba_model2)
 
+#visuals
 plot(woba_model2, scheme = 1)
 plot(woba_model2, scheme = 2)
 
@@ -71,4 +76,65 @@ vis.gam(x=woba_model2,
         contour.col = "black", 
         nlevels = 20) #number of lines
 
+#logistic model for predicting whether a player hit a homerun---------------
+
+#add a binary homerun variable to training and test data 
+train_mlb_data <- train_mlb_data %>%
+  mutate(homerun = events == "home_run")
+test_mlb_data <- test_mlb_data %>%
+  mutate(homerun = events == "home_run")
+
+#fit model 
+homerun_mod <- gam(homerun ~ s(launch_speed, launch_angle), data = train_mlb_data, 
+               family = binomial, 
+               method = "REML")
+summary(homerun_mod)
+
+#test model on jason heyward
+jason_heyward2 <- test_mlb_data %>%
+  filter(player_name == "Heyward, Jason", description == "hit_into_play") 
+
+jhey <- tibble(hr.preds = predict(homerun_mod, newdata = jason_heyward2))
+jhey_preds <- plogis(jhey$hr.preds)
+mean(jhey_preds, na.rm = TRUE) #heyward has a 2.53% likelihood of hitting homerun 
+#when he puts the ball in play
+
+#test model on mike trout
+mike_trout2 <- test_mlb_data %>%
+  filter(player_name == "Trout, Mike", description == "hit_into_play") 
+
+mtrout <- tibble(hr.preds = predict(homerun_mod, newdata = mike_trout2))
+mtrout_preds <- plogis(mtrout$hr.preds)
+mean(mtrout_preds, na.rm = TRUE) #trout has a 15% likelihood of hitting homerun 
+#when he puts the ball in play
+
+#test model on joey gallo 
+joey_gallo <- test_mlb_data %>%
+  filter(player_name == "Gallo, Joey", description == "hit_into_play") 
+joeyg <- tibble(hr.preds = predict(homerun_mod, newdata = joey_gallo))
+joeyg_preds <- plogis(joeyg$hr.preds)
+mean(joeyg_preds, na.rm = TRUE) #gallo 15.8% likelihood
+
+#test model on trea turner
+trea_turner <- test_mlb_data %>%
+  filter(player_name == "Turner, Trea", description == "hit_into_play") 
+tt <- tibble(hr.preds = predict(homerun_mod, newdata = trea_turner))
+tt_preds <- plogis(tt$hr.preds)
+mean(tt_preds, na.rm = TRUE) #turner with 5.4% probability 
+
+#see their actual percentages 
+hrs_by_players <- batter_all_1921hp %>%
+  mutate(homerun = events == "home_run") %>%
+  filter(player_name %in% c("Trout, Mike", "Turner, Trea", "Heyward, Jason", "Gallo, Joey")) %>%
+  group_by(player_name) %>%
+  summarize(hrs = sum(homerun))
+
+into_play <- batter_all_1921hp %>%
+  filter(player_name %in% c("Trout, Mike", "Turner, Trea", "Heyward, Jason", "Gallo, Joey")) %>%
+  group_by(player_name) %>%
+  count()
+
+hrs_by_players %>%
+  inner_join(into_play, by = "player_name") %>%
+  mutate(hr_percent = hrs/n)
 
