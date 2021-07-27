1cb344ee613ba3d5cd738ed2eaf81b278e1599c2
Erin Franke <efranke@macalester.edu>
2021-07-27 18:08:25 -0500

    updated changes to shiny app code (still not working)

--- woba_shinyapp/app.R
+++ woba_shinyapp/app.R
@@ -1,5 +1,6 @@
 library(shiny)
 library(tidyverse)
+library(readr)
 library(baseballr)
 library(mgcv)
 library(ggfortify)
@@ -39,166 +40,172 @@ predicted_LA <- lm(launch_angle ~ attack_angle + plate_z, data=batter_all_2019hp
 final_woba_model2 <- gam(woba_value ~ s(launch_angle, launch_speed, k=200), data = batter_all_2019hp, 
                          method = "REML")
 
-predicted_LA_adjust_attack <- function(woba_model, LA_model, player_data, orig_woba, orig_attack, attack)
-{
-  
-  # Model the predicted angles given the original attack angle
-  pred_angles <- tibble(lm.preds = predict(LA_model, newdata = player_data))
-  pred_angles <- pred_angles %>% mutate(noise = rnorm(n = length(pred_angles$lm.preds), mean = 0, 
-                                                      sd = sigma(LA_model)), 
-                                        launch_angle = lm.preds + noise)
-  
-  # Need to sample the data for each predicted angle to find what exit velocity we would give it
-  for(i in 1:length(pred_angles$launch_angle)){
-    
-    # Check all predicted launch angles and if it outside of 2 sd of their mean - replace with 
-    #the cap (plus or minus)
-    if(pred_angles$launch_angle[i] < (mean(player_data$launch_angle)-2*sd(player_data$launch_angle))){
-      pred_angles$launch_angle[i] <- (mean(player_data$launch_angle)-2*sd(player_data$launch_angle))
-    }
-    else if(pred_angles$launch_angle[i] > (mean(player_data$launch_angle)+2*sd(player_data$launch_angle))){
-      pred_angles$launch_angle[i] <- (mean(player_data$launch_angle)+2*sd(player_data$launch_angle))
-    }
-    
-    # Filter for the player's launch angles plus or minus 3 degrees above the ACTUAL LA
-    hits_at_angle <- player_data %>% 
-      filter(cleaned_launch_angle <= orig_attack+3 & launch_angle >= 
-               orig_attack-3 & !is.na(launch_speed))
-    # Randomly sample 1 exit velocity form similar hits
-    EV_sample_index <- sample(1:nrow(hits_at_angle), 1, replace = TRUE)
-    pred_EV <- hits_at_angle[EV_sample_index,] 
-    # Add that launch speed to vector as the predicted launch speed 
-    EV_vector1 <- c(EV_vector1, pred_EV$launch_speed)
-  }
-  
-  # Merge predicted launch angle and sampled exit velocity
-  modeled_data <- tibble(launch_angle = pred_angles$launch_angle, launch_speed = EV_vector1)
-  preds1 <- tibble(gam.preds = predict(woba_model, newdata = modeled_data))  
-  xwOBA1 <- mean(preds1$gam.preds, na.rm = TRUE)
-  
-  # Repeat with a +3 attack angle
-  plus_one_attack <- player_data
-  plus_one_attack$attack_angle <- plus_one_attack$attack_angle + 3
-  
-  pred_angles2 <- tibble(lm.preds = predict(LA_model, newdata = plus_one_attack))
-  pred_angles2 <- pred_angles2 %>% mutate(noise = rnorm(n = length(pred_angles2$lm.preds), mean = 0, 
-                                                        sd = sigma(LA_model)), 
-                                          launch_angle = lm.preds + noise)
-  
-  # Need to sample the data for each predicted angle to find what exit velocity we would give it
-  for(i in 1:length(pred_angles2$launch_angle)){
-    
-    # Check all predicted launch angles and if it outside of 2 sd of their mean - replace with 
-    #the cap (plus or minus)
-    if(pred_angles2$launch_angle[i] < (mean(player_data$launch_angle)-2*sd(player_data$launch_angle))){
-      pred_angles2$launch_angle[i] <- (mean(player_data$launch_angle)-2*sd(player_data$launch_angle))
-    }
-    else if(pred_angles2$launch_angle[i] > (mean(player_data$launch_angle)+2*sd(player_data$launch_angle))){
-      pred_angles2$launch_angle[i] <- (mean(player_data$launch_angle)+2*sd(player_data$launch_angle))
-    }
-    
-    # Filter for the player's launch angles plus or minus 3 degrees above the ACTUAL LA
-    hits_at_angle <- player_data %>% 
-      filter(cleaned_launch_angle <= orig_attack+3 & launch_angle >= 
-               orig_attack-3 & !is.na(launch_speed))
-    # Randomly sample 1 exit velocity form similar hits
-    EV_sample_index <- sample(1:nrow(hits_at_angle), 1, replace = TRUE)
-    pred_EV <- hits_at_angle[EV_sample_index,] 
-    # Add that launch speed to vector as the predicted launch speed 
-    EV_vector2 <- c(EV_vector2, pred_EV$launch_speed)
-  }
-  
-  modeled_data_plus_one <- tibble(launch_angle = pred_angles2$launch_angle, launch_speed = EV_vector2)
-  preds2 <- tibble(gam.preds = predict(woba_model, newdata = modeled_data_plus_one))  
-  xwOBA2 <- mean(preds2$gam.preds, na.rm = TRUE)
-  
-  # Repeat with a -3 attack angle
-  minus_one_attack <- player_data
-  minus_one_attack$attack_angle <- minus_one_attack$attack_angle - 3
-  
-  pred_angles3 <- tibble(lm.preds = predict(LA_model, newdata = minus_one_attack))
-  pred_angles3 <- pred_angles3 %>% mutate(noise = rnorm(n = length(pred_angles3$lm.preds), mean = 0, 
-                                                        sd = sigma(LA_model)), 
-                                          launch_angle = lm.preds + noise)
-  
-  # Need to sample the data for each predicted angle to find what exit velocity we would give it
-  for(i in 1:length(pred_angles3$launch_angle)){
-    
-    # Check all predicted launch angles and if it outside of 2 sd of their mean - replace with 
-    #the cap (plus or minus)
-    if(pred_angles3$launch_angle[i] < (mean(player_data$launch_angle)-2*sd(player_data$launch_angle))){
-      pred_angles3$launch_angle[i] <- (mean(player_data$launch_angle)-2*sd(player_data$launch_angle))
-    }
-    else if(pred_angles3$launch_angle[i] > (mean(player_data$launch_angle)+2*sd(player_data$launch_angle))){
-      pred_angles3$launch_angle[i] <- (mean(player_data$launch_angle)+2*sd(player_data$launch_angle))
-    }
-    
-    # Filter for the player's launch angles plus or minus 3 degrees above the ACTUAL LA
-    hits_at_angle <- player_data %>% 
-      filter(cleaned_launch_angle <= orig_attack+3 & launch_angle >= 
-               orig_attack-3 & !is.na(launch_speed))
-    # Randomly sample 1 exit velocity form similar hits
-    EV_sample_index <- sample(1:nrow(hits_at_angle), 1, replace = TRUE)
-    pred_EV <- hits_at_angle[EV_sample_index,] 
-    # Add that launch speed to vector as the predicted launch speed 
-    EV_vector3 <- c(EV_vector3, pred_EV$launch_speed)
-  }
-  
-  modeled_data_minus_one <- tibble(launch_angle = pred_angles3$launch_angle, launch_speed = EV_vector3)
-  preds3 <- tibble(gam.preds = predict(woba_model, newdata = modeled_data_minus_one))  
-  xwOBA3 <- mean(preds3$gam.preds, na.rm = TRUE)
-  
-  
-  # print(player_data$attack_angle[1])
-  # print(pred_angles)    
-  # print(modeled_data)
-  # print(min(modeled_data$launch_angle))
-  # print(max(modeled_data$launch_angle))
-  # print(plus_one_attack$attack_angle[1])
-  # print(pred_angles2)
-  # print(modeled_data_plus_one)
-  # print(min(modeled_data_plus_one$launch_angle))
-  # print(max(modeled_data_plus_one$launch_angle))
-  # print(minus_one_attack$attack_angle[1])
-  # print(pred_angles3)
-  # print(modeled_data_minus_one)
-  # print(min(modeled_data_minus_one$launch_angle))
-  # print(max(modeled_data_minus_one$launch_angle))
-  
-  
-  # If original < +1
-  if(xwOBA1 < xwOBA2){
-    # Recursively call with +1 attack angle data
-    predicted_LA_adjust_attack(woba_model, LA_model, plus_one_attack, orig_woba, orig_attack, attack+3)
-  }
-  # Else if original < -1
-  else if (xwOBA1 < xwOBA3){
-    # Recursively call with -1 attack angle data
-    predicted_LA_adjust_attack(woba_model, LA_model, minus_one_attack, orig_woba, orig_attack, attack-3)
-  }
-  # Else
-  else{
-    # Return the orig_woba, xwOBA, orig_attack, and attack angles
-    return (tibble(original_woba = orig_woba, predicted_woba = xwOBA1, original_attack = orig_attack, 
-                   reccomended_attack = attack)[1,])
-  }
-  
-}
+# set.seed(2021)
+# # Create empty vectors of exit velocities 
+# EV_vector1 <- vector()    #for no change in attack angle
+# EV_vector2 <- vector()    #for plus one attack angle
+# EV_vector3 <- vector()    #for minus one attack angle
 
-# Function to call adjust attack multiple times ---------------------------
-
-repeat_adjust_attack <- function(player_data, player_woba){
-  final_results <-tibble(predicted_LA_adjust_attack(final_woba_model2, predicted_LA, player_data, player_woba, 
-                                                    player_data$attack_angle, player_data$attack_angle))
-  for(i in 1:99){
-    results <- predicted_LA_adjust_attack(final_woba_model2, predicted_LA, player_data, player_woba, 
-                                          player_data$attack_angle, player_data$attack_angle)
-    final_results <- bind_rows(final_results, results)
-  }
-  averages <- colMeans(final_results)
-  final_results <- bind_rows(final_results, averages)
-  return(tail(final_results))
-}
+# predicted_LA_adjust_attack <- function(woba_model, LA_model, player_data, orig_woba, orig_attack, attack)
+# {
+#   
+#   # Model the predicted angles given the original attack angle
+#   pred_angles <- tibble(lm.preds = predict(LA_model, newdata = player_data))
+#   pred_angles <- pred_angles %>% mutate(noise = rnorm(n = length(pred_angles$lm.preds), mean = 0, 
+#                                                       sd = sigma(LA_model)), 
+#                                         launch_angle = lm.preds + noise)
+#   
+#   # Need to sample the data for each predicted angle to find what exit velocity we would give it
+#   for(i in 1:length(pred_angles$launch_angle)){
+#     
+#     # Check all predicted launch angles and if it outside of 2 sd of their mean - replace with 
+#     #the cap (plus or minus)
+#     if(pred_angles$launch_angle[i] < (mean(player_data$launch_angle)-2*sd(player_data$launch_angle))){
+#       pred_angles$launch_angle[i] <- (mean(player_data$launch_angle)-2*sd(player_data$launch_angle))
+#     }
+#     else if(pred_angles$launch_angle[i] > (mean(player_data$launch_angle)+2*sd(player_data$launch_angle))){
+#       pred_angles$launch_angle[i] <- (mean(player_data$launch_angle)+2*sd(player_data$launch_angle))
+#     }
+#     
+#     # Filter for the player's launch angles plus or minus 3 degrees above the ACTUAL LA
+#     hits_at_angle <- player_data %>% 
+#       filter(cleaned_launch_angle <= orig_attack+3 & launch_angle >= 
+#                orig_attack-3 & !is.na(launch_speed))
+#     # Randomly sample 1 exit velocity form similar hits
+#     EV_sample_index <- sample(1:nrow(hits_at_angle), 1, replace = TRUE)
+#     pred_EV <- hits_at_angle[EV_sample_index,] 
+#     # Add that launch speed to vector as the predicted launch speed 
+#     EV_vector1 <- c(EV_vector1, pred_EV$launch_speed)
+#   }
+#   
+#   # Merge predicted launch angle and sampled exit velocity
+#   modeled_data <- tibble(launch_angle = pred_angles$launch_angle, launch_speed = EV_vector1)
+#   preds1 <- tibble(gam.preds = predict(woba_model, newdata = modeled_data))  
+#   xwOBA1 <- mean(preds1$gam.preds, na.rm = TRUE)
+#   
+#   # Repeat with a +3 attack angle
+#   plus_one_attack <- player_data
+#   plus_one_attack$attack_angle <- plus_one_attack$attack_angle + 3
+#   
+#   pred_angles2 <- tibble(lm.preds = predict(LA_model, newdata = plus_one_attack))
+#   pred_angles2 <- pred_angles2 %>% mutate(noise = rnorm(n = length(pred_angles2$lm.preds), mean = 0, 
+#                                                         sd = sigma(LA_model)), 
+#                                           launch_angle = lm.preds + noise)
+#   
+#   # Need to sample the data for each predicted angle to find what exit velocity we would give it
+#   for(i in 1:length(pred_angles2$launch_angle)){
+#     
+#     # Check all predicted launch angles and if it outside of 2 sd of their mean - replace with 
+#     #the cap (plus or minus)
+#     if(pred_angles2$launch_angle[i] < (mean(player_data$launch_angle)-2*sd(player_data$launch_angle))){
+#       pred_angles2$launch_angle[i] <- (mean(player_data$launch_angle)-2*sd(player_data$launch_angle))
+#     }
+#     else if(pred_angles2$launch_angle[i] > (mean(player_data$launch_angle)+2*sd(player_data$launch_angle))){
+#       pred_angles2$launch_angle[i] <- (mean(player_data$launch_angle)+2*sd(player_data$launch_angle))
+#     }
+#     
+#     # Filter for the player's launch angles plus or minus 3 degrees above the ACTUAL LA
+#     hits_at_angle <- player_data %>% 
+#       filter(cleaned_launch_angle <= orig_attack+3 & launch_angle >= 
+#                orig_attack-3 & !is.na(launch_speed))
+#     # Randomly sample 1 exit velocity form similar hits
+#     EV_sample_index <- sample(1:nrow(hits_at_angle), 1, replace = TRUE)
+#     pred_EV <- hits_at_angle[EV_sample_index,] 
+#     # Add that launch speed to vector as the predicted launch speed 
+#     EV_vector2 <- c(EV_vector2, pred_EV$launch_speed)
+#   }
+#   
+#   modeled_data_plus_one <- tibble(launch_angle = pred_angles2$launch_angle, launch_speed = EV_vector2)
+#   preds2 <- tibble(gam.preds = predict(woba_model, newdata = modeled_data_plus_one))  
+#   xwOBA2 <- mean(preds2$gam.preds, na.rm = TRUE)
+#   
+#   # Repeat with a -3 attack angle
+#   minus_one_attack <- player_data
+#   minus_one_attack$attack_angle <- minus_one_attack$attack_angle - 3
+#   
+#   pred_angles3 <- tibble(lm.preds = predict(LA_model, newdata = minus_one_attack))
+#   pred_angles3 <- pred_angles3 %>% mutate(noise = rnorm(n = length(pred_angles3$lm.preds), mean = 0, 
+#                                                         sd = sigma(LA_model)), 
+#                                           launch_angle = lm.preds + noise)
+#   
+#   # Need to sample the data for each predicted angle to find what exit velocity we would give it
+#   for(i in 1:length(pred_angles3$launch_angle)){
+#     
+#     # Check all predicted launch angles and if it outside of 2 sd of their mean - replace with 
+#     #the cap (plus or minus)
+#     if(pred_angles3$launch_angle[i] < (mean(player_data$launch_angle)-2*sd(player_data$launch_angle))){
+#       pred_angles3$launch_angle[i] <- (mean(player_data$launch_angle)-2*sd(player_data$launch_angle))
+#     }
+#     else if(pred_angles3$launch_angle[i] > (mean(player_data$launch_angle)+2*sd(player_data$launch_angle))){
+#       pred_angles3$launch_angle[i] <- (mean(player_data$launch_angle)+2*sd(player_data$launch_angle))
+#     }
+#     
+#     # Filter for the player's launch angles plus or minus 3 degrees above the ACTUAL LA
+#     hits_at_angle <- player_data %>% 
+#       filter(cleaned_launch_angle <= orig_attack+3 & launch_angle >= 
+#                orig_attack-3 & !is.na(launch_speed))
+#     # Randomly sample 1 exit velocity form similar hits
+#     EV_sample_index <- sample(1:nrow(hits_at_angle), 1, replace = TRUE)
+#     pred_EV <- hits_at_angle[EV_sample_index,] 
+#     # Add that launch speed to vector as the predicted launch speed 
+#     EV_vector3 <- c(EV_vector3, pred_EV$launch_speed)
+#   }
+#   
+#   modeled_data_minus_one <- tibble(launch_angle = pred_angles3$launch_angle, launch_speed = EV_vector3)
+#   preds3 <- tibble(gam.preds = predict(woba_model, newdata = modeled_data_minus_one))  
+#   xwOBA3 <- mean(preds3$gam.preds, na.rm = TRUE)
+#   
+#   
+#   # print(player_data$attack_angle[1])
+#   # print(pred_angles)    
+#   # print(modeled_data)
+#   # print(min(modeled_data$launch_angle))
+#   # print(max(modeled_data$launch_angle))
+#   # print(plus_one_attack$attack_angle[1])
+#   # print(pred_angles2)
+#   # print(modeled_data_plus_one)
+#   # print(min(modeled_data_plus_one$launch_angle))
+#   # print(max(modeled_data_plus_one$launch_angle))
+#   # print(minus_one_attack$attack_angle[1])
+#   # print(pred_angles3)
+#   # print(modeled_data_minus_one)
+#   # print(min(modeled_data_minus_one$launch_angle))
+#   # print(max(modeled_data_minus_one$launch_angle))
+#   
+#   
+#   # If original < +1
+#   if(xwOBA1 < xwOBA2){
+#     # Recursively call with +1 attack angle data
+#     predicted_LA_adjust_attack(woba_model, LA_model, plus_one_attack, orig_woba, orig_attack, attack+3)
+#   }
+#   # Else if original < -1
+#   else if (xwOBA1 < xwOBA3){
+#     # Recursively call with -1 attack angle data
+#     predicted_LA_adjust_attack(woba_model, LA_model, minus_one_attack, orig_woba, orig_attack, attack-3)
+#   }
+#   # Else
+#   else{
+#     # Return the orig_woba, xwOBA, orig_attack, and attack angles
+#     return (tibble(original_woba = orig_woba, predicted_woba = xwOBA1, original_attack = orig_attack, 
+#                    reccomended_attack = attack)[1,])
+#   }
+#   
+# }
+# 
+# # Function to call adjust attack multiple times ---------------------------
+# 
+# # repeat_adjust_attack <- function(player_data, player_woba){
+# #   final_results <-tibble(predicted_LA_adjust_attack(final_woba_model2, predicted_LA, player_data, player_woba, 
+# #                                                     player_data$attack_angle, player_data$attack_angle))
+# #   for(i in 1:99){
+# #     results <- predicted_LA_adjust_attack(final_woba_model2, predicted_LA, player_data, player_woba, 
+# #                                           player_data$attack_angle, player_data$attack_angle)
+# #     final_results <- bind_rows(final_results, results)
+# #   }
+# #   averages <- colMeans(final_results)
+# #   final_results <- bind_rows(final_results, averages)
+# #   return(tail(final_results))
+# # }
 
 # Testing all possible attack angles --------------------------------------
 
