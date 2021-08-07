996e3df9e10161c1e3227e2760a360f7b74de919
Sarah Sult <ssult@wustl.edu>
2021-08-07 17:43:50 -0500

    attempt at a batting avg/attack angle model

--- sarah-code/Logistic_model.R
+++ sarah-code/Logistic_model.R
@@ -2,6 +2,7 @@
 #comes in
 
 library(tidyverse)
+library(mgcv)
 
 
 # Loading Data ------------------------------------------------------------
@@ -12,6 +13,7 @@ batter_all_2018 <- read_rds("private_data/all2018data.rds")
 batter_all_2019 <- read_rds("private_data/all2019data.rds")
 batter_all_2020 <- read_rds("private_data/all2020data.rds")
 batter_all_2021 <- read_rds("private_data/all2021data.rds")
+
 '%!in%' <- Negate('%in%')
 
 batter_all_2016 <- batter_all_2016 %>%
@@ -116,16 +118,22 @@ batter_all_2019_logit <- batter_all_2019_logit %>%
 batter_all_1621_logit <- batter_all_1621_logit %>%
   mutate(is_hit_into_play = ifelse(description == "hit_into_play", 1, 0))
 
+#read in the stats and add the player's attack angle
+stats_2019 <- read_csv("public_data/expected_stats2019.csv")
+stats_2019 <- stats_2019 %>% mutate(player_name = paste(last_name, first_name, sep=", "))
+name_and_attack <- batter_all_2019_logit %>% select(player_name, attack_angle)
+
+avg_and_attack_modeling <- stats_2019 %>% left_join(name_and_attack, by = c("player_name")) %>% distinct()
 
 # Creating initial glm ----------------------------------------------------
 
 init_logit <- glm(is_hit_into_play ~ attack_angle, #+ plate_z + release_speed, 
-                  data = batter_all_1621_logit, family = "binomial")
+                  data = batter_all_2019_logit, family = "binomial")
 summary(init_logit)
 
-pred_hit_outcomes <- ifelse(init_logit$fitted.values > 0.5, "hit", "no hit")
+#pred_hit_outcomes <- ifelse(init_logit$fitted.values > 0.5, "hit", "no hit")
 
-table("Predictions" = pred_hit_outcomes, "Observed" = batter_all_1621_logit$is_hit_into_play)
+#table("Predictions" = pred_hit_outcomes, "Observed" = batter_all_2019_logit$is_hit_into_play)
 max(init_logit$fitted.values)
 
 #I think there are so few hit into play that when you have a a similar attack angle/plate height 
@@ -134,6 +142,13 @@ max(init_logit$fitted.values)
 #could we use the low probabilities as the prob of selection and then select the number of balls hit into
 #play based on a modeled batting average for that launch angle?????
 
+avg_and_attack_modeling %>%
+  ggplot(aes(x=attack_angle, y=ba))+
+  geom_line() + geom_smooth() 
+
+init_avg_model <- gam(ba ~ s(attack_angle, k=125), data=avg_and_attack_modeling)
+summary(init_avg_model)
+gam.check(init_avg_model, k.sample = 50000, k.rep = 250)
 
 # Model strike percentage  ---------------------------------------------
 strike_modeling %>%
