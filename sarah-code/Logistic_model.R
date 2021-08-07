3b8a164d6c1b2d3a36175c974f319ee87e2d8e66
Sarah Sult <ssult@wustl.edu>
2021-08-07 14:09:18 -0500

    still trying to model what balls they would hit at a different swing plane

--- sarah-code/Logistic_model.R
+++ sarah-code/Logistic_model.R
@@ -6,11 +6,10 @@ library(tidyverse)
 
 # Loading Data ------------------------------------------------------------
 
-batter_all_2019 <- read_rds("private_data/all2019data.rds")
-
 batter_all_2016 <- read_rds("private_data/all2016data.rds")
 batter_all_2017 <- read_rds("private_data/all2017data.rds")
 batter_all_2018 <- read_rds("private_data/all2018data.rds")
+batter_all_2019 <- read_rds("private_data/all2019data.rds")
 batter_all_2020 <- read_rds("private_data/all2020data.rds")
 batter_all_2021 <- read_rds("private_data/all2021data.rds")
 '%!in%' <- Negate('%in%')
@@ -31,12 +30,7 @@ batter_all_2021 <- batter_all_2021 %>%
 batter_all_1621 <- bind_rows(batter_all_2016, batter_all_2017, batter_all_2018, 
                              batter_all_2019, batter_all_2020, batter_all_2021)
 
-#create the main dataset  (ERIN)-----------------------------------------------------
-# I want a dataset that shows a player's attack angle (their median launch angle
-# in their top 10% of exit velocities) for each season where they have at least 50
-# batted balls, their average launch angle in each season, their number of plate 
-# appearances and number of batted balls, number of strikeouts/strikeout percentage, 
-# and wOBA. 
+#GET THE STRIKE PERCENTAGE FOR EACH PLAYER FOR EACH YEAR
 
 #find each player's attack angle in each season
 attack_angles <- batter_all_1621 %>%
@@ -66,31 +60,39 @@ batted_balls <- batter_all_1621 %>%
   count() %>%
   rename(balls_in_play = n)
 
-#find each player's number of strikeouts
-strikeouts <- batter_all_1621 %>%
+#find each player's number of strikes
+strikes <- batter_all_1621 %>%
   group_by(player_name, year) %>%
-  filter(events %in% c("strikeout", "strikeout_double_play")) %>%
+  filter(description %!in% c("hit_into_play", "ball", "hit_by_pitch")) %>%
   count() %>%
-  rename(K=n)
+  rename(strikes=n)     
+  
 
 #find each player's wOBA each season
 wOBAs <-  batter_all_1621 %>%
   group_by(player_name, year) %>%
   summarize(woba = mean(woba_value, na.rm = TRUE))
 
+#Get total number of pitches seen by a player during a year
+total_pitches <- batter_all_1621 %>%
+  group_by(player_name, year) %>%
+  count() %>%
+  rename(total_pitches = n)
+
 #create joined data set
-strikeout_eda <- plate_appearances %>%
-  left_join(strikeouts, by = c("player_name", "year")) %>%
+strike_modeling <- plate_appearances %>%
+  left_join(strikes, by = c("player_name", "year")) %>%
   left_join(attack_angles, by=c("player_name", "year")) %>%
   left_join(launch_angles, by=c("player_name", "year")) %>%
   left_join(wOBAs, by = c("player_name", "year")) %>%
   left_join(batted_balls, by = c("player_name", "year")) %>%
-  mutate(k_percent = K/n_pa) %>%
-  filter(balls_in_play >=50)
+  left_join(total_pitches, by = c("player_name", "year")) %>%
+  mutate(strike_percent = strikes/total_pitches) %>%
+  filter(attack_angle <= 30 & attack_angle >=0)
 
-#Creating a column with corresponding attack angle per player then filtering so that variables we (ME) ----
+#Creating a column with corresponding attack angle per player then filtering so that the variables we 
 #use in the model do not have NA 
-batter_all_2019 <- batter_all_2019 %>% 
+batter_all_2019_logit <- batter_all_2019 %>% 
   group_by(player_name) %>% 
   filter(launch_speed <= 120 -.02 * abs(launch_angle - 12)^1.7) %>%
   filter(launch_speed >= quantile(launch_speed, .9, na.rm = TRUE)) %>%
@@ -99,7 +101,7 @@ batter_all_2019 <- batter_all_2019 %>%
   filter(!is.na(plate_z), !is.na(attack_angle), !is.na(description), !is.na(release_speed),
          !is.na(pitch_type)) #MIGHT NEED TO ADD/CHANGE VARS DEPENDING ON WHAT WE USE IN THE MODEL
 
-batter_all_1621 <- batter_all_1621 %>% 
+batter_all_1621_logit <- batter_all_1621 %>% 
   group_by(player_name) %>% 
   filter(launch_speed <= 120 -.02 * abs(launch_angle - 12)^1.7) %>%
   filter(launch_speed >= quantile(launch_speed, .9, na.rm = TRUE)) %>%
@@ -109,31 +111,37 @@ batter_all_1621 <- batter_all_1621 %>%
          !is.na(pitch_type))
 
 #Adding a yes/no column for if a ball was hit into play
-batter_all_1621 <- batter_all_1621 %>%
+batter_all_2019_logit <- batter_all_2019_logit %>%
+  mutate(is_hit_into_play = ifelse(description == "hit_into_play", 1, 0))
+batter_all_1621_logit <- batter_all_1621_logit %>%
   mutate(is_hit_into_play = ifelse(description == "hit_into_play", 1, 0))
 
 
 # Creating initial glm ----------------------------------------------------
 
 init_logit <- glm(is_hit_into_play ~ attack_angle, #+ plate_z + release_speed, 
-                  data = batter_all_1621, family = "binomial")
+                  data = batter_all_1621_logit, family = "binomial")
 summary(init_logit)
 
 pred_hit_outcomes <- ifelse(init_logit$fitted.values > 0.5, "hit", "no hit")
 
-table("Predictions" = pred_hit_outcomes, "Observed" = batter_all_1621$is_hit_into_play)
+table("Predictions" = pred_hit_outcomes, "Observed" = batter_all_1621_logit$is_hit_into_play)
 max(init_logit$fitted.values)
 
 #I think there are so few hit into play that when you have a a similar attack angle/plate height 
 #combination the not hit into play lowers the prob of a hit
 
-#Erin's logit model--------------------------------------------------------
-#Create a glm for strikeOUT probability
-k_probability <- glm(cbind(K, n_pa-K)~attack_angle, family="binomial", data=strikeout_eda)
-summary(k_probability)
+#could we use the low probabilities as the prob of selection and then select the number of balls hit into
+#play based on a modeled batting average for that launch angle?????
 
-#Try a random forrest or XGBoost -----------------------------------------
 
+# Model strike percentage  ---------------------------------------------
+strike_modeling %>%
+  ggplot(aes(x=attack_angle, y=strike_percent))+
+  geom_line() + geom_smooth() #practically no relationship...so the model is useless
+
+strike_percent_init_model <- lm(strike_percent ~ attack_angle, data=strike_modeling)
+summary(strike_percent_init_model)
 
 
 
