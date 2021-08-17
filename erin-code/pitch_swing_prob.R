7ec550f37ca129cdbf43456e8dbccb59b4f7921c
Erin Franke <efranke@macalester.edu>
2021-08-16 21:02:31 -0500

    looking at swing probability eda

--- erin-code/pitch_swing_prob.R
+++ erin-code/pitch_swing_prob.R
@@ -9,6 +9,7 @@ library(tidyverse)
 library(plotly)
 library(broom)
 library(mgcv)
+library(ggtext)
 '%!in%' <- Negate('%in%')
 
 #Loading Data
@@ -21,19 +22,46 @@ batter_all_2021 <- read_rds("private_data/all2021data.rds")
 ####################################################################################################################################
 # EDA TO EXPLORE WHAT PITCHES ARE SWUNG AT 
 
-# First, denote whether a pitch was swung at, create a varaible called swung_at that denotes 
+# First, denote whether a pitch was swung at, create a variable called swung_at that denotes 
 #with a 1 if the pitch was swung at and a 0 if the pitch was not swung at. 
 swing_prob_data <- batter_all_2019 %>%
   mutate(swung_at = case_when(description %in% c("ball", "blocked_ball", "called_strike", "hit_by_pitch") ~ 0, 
                               description %in% c("foul", "foul_tip", "hit_into_play", "swinging_strike", "swinging_strike_blocked") ~ 1, 
                               TRUE ~ 2), 
-         swung_at = as.factor(swung_at)) %>%
+         swung_at = as.factor(swung_at), 
+         pitch_type = case_when(pitch_type %in% c("CH", "EP") ~ "Offspeed", 
+                                       pitch_type %in% c("CS", "CU", "KC", "KN", "SC", "SL") ~ "Breaking", 
+                                       pitch_type %in% c("FA", "FO", "FS", "FT", "SI", "FC", "FF") ~ "Fastball", 
+                                       TRUE ~ pitch_type), 
+         in_horizontal_zone = case_when(abs(plate_x) > 11/12 ~ 0, 
+                                        TRUE ~ 1), 
+         in_horizontal_zone = as.factor(in_horizontal_zone)) %>%
   filter(swung_at %in% c(0, 1)) %>%
-  select(swung_at, everything())
+  select(swung_at, pitch_type, in_horizontal_zone, everything())
+
+# Visualization that selects 2000 pitches and creates a scatterplot of zone coordinates showing what 
+#was swung at and what was taken
 swing_prob_data %>%
+  head(2000)%>%
   ggplot(aes(x=plate_x, y=plate_z, color=swung_at)) +
   geom_point(alpha=0.7)+
-  theme_minimal()
+  geom_vline(xintercept = 11/12, linetype = "dashed")+
+  geom_vline(xintercept = -11/12, linetype = "dashed")+
+  theme_minimal()+
+  scale_color_manual(values = c("firebrick", "cadetblue3"))+
+  labs(x="Horizontal pitch location (ft)", y="Pitch height (ft)", 
+       title = "MLB Hitter Pitch Selection: <strong><span style='color:firebrick'>Takes</span></strong></b> versus <strong><span style='color:cadetblue3'>swings</span></strong></b>")+
+  theme(plot.title.position = "plot",
+        plot.title = element_markdown(size = 10), 
+        axis.title = element_markdown(size=9),
+        legend.position = "none")
 
+#For pitches swung at outside of the horizontal strike zone (still working on this one)
+swing_prob_data %>%
+  filter(!is.na(pitch_type)) %>%
+  ggplot(aes(x=in_horizontal_zone, fill=pitch_type))+
+  geom_bar(position = "fill")+
+  facet_wrap(~swung_at)+
+  theme_minimal()
 
   
