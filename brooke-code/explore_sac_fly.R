2847288a0bb38b23bfa35d3fccb9a7e2ad3bd30c
Brooke Coneeny <77593987+brooke-coneeny@users.noreply.github.com>
2021-09-06 22:45:48 -0400

    working on categorizing predicted woba values into different types of hits

--- brooke-code/explore_sac_fly.R
+++ brooke-code/explore_sac_fly.R
@@ -11,28 +11,11 @@ library(mgcv)
 # Load in data 
 trout_data <- read_rds("public_data/mtrout_sample_hits.rds")
 heyward_data <- read_rds("public_data/jhey_sample_hits.rds")
+kemp_data <- read_rds("public_data/tkemp_sample_hits.rds")
 
 # Load in the models
 woba_model <- read_rds("public_data/woba_model.rds")
 linear_model <- read_rds("public_data/LA_model.rds")
-
-####################################################################################################################################
-
-# Find the number of sac flies at each attack angle 
-sac_fly_data <- heyward_data %>%
-  mutate(event_sac_fly = case_when(events == "sac_fly" ~ 1, 
-                                   TRUE ~0)) %>%
-  group_by(attack_angle, event_sac_fly) %>%
-  count() %>%
-  pivot_wider(id_cols = attack_angle, names_from = event_sac_fly, values_from = n) %>%
-  rename(`sac`=`1`, `other`=`0`) %>%
-  mutate(sacs_per_hip = sac/(sac+other)) %>%
-  select(attack_angle, sacs_per_hip)
-
-# Add this column to data set 
-batter_data <- heyward_data %>%
-  left_join(sac_fly_data, by=c("attack_angle")) %>%
-  filter(!is.na(events))
 
 ####################################################################################################################################
 
@@ -53,7 +36,7 @@ clean_edges <- function (data){
 
 ####################################################################################################################################
 
-sampling <- function(pred_angles, player_data, EV_vector, orig_attack){
+predicted_angles <- function(pred_angles, player_data){
   # Check all predicted launch angles and if it outside of 2 sd of their mean - replace with the cap (plus or minus)
   for(i in 1:length(pred_angles$launch_angle)){
     if(pred_angles$launch_angle[i] < (mean(player_data$launch_angle)-2*sd(player_data$launch_angle))){
@@ -65,139 +48,140 @@ sampling <- function(pred_angles, player_data, EV_vector, orig_attack){
     else{
       pred_angles$launch_angle[i] <- pred_angles$launch_angle[i]
     }
-    
+  }
+}
+
+####################################################################################################################################
+
+sampling <- function(batter_data, EV_vector){
+  
+  # Repeat for each swing since attack angle differs
+  for(i in 1:nrow(batter_data)) {
     # Need to sample the data for each predicted angle to find what exit velocity we would give it 
     # Filter for the player's launch angles plus or minus 3 degrees around the attack angle 
-    hits_at_angle <- player_data %>% 
-      filter(cleaned_launch_angle <= orig_attack +3 & cleaned_launch_angle >= orig_attack -3 & !is.na(launch_speed))
+    hits_at_angle <- batter_data %>% 
+      filter(cleaned_launch_angle <= batter_data$attack_angle[i] +3 & 
+               cleaned_launch_angle >= batter_data$attack_angle[i] -3 & 
+               !is.na(launch_speed))
     # Randomly sample 1 exit velocity form similar hits
     EV_sample_index <- sample(1:nrow(hits_at_angle), 1, replace = TRUE)
     pred_EV <- hits_at_angle[EV_sample_index,] 
     # Add that launch speed to vector as the predicted launch speed 
     EV_vector <- c(EV_vector, pred_EV$launch_speed)
   }
+
   return(EV_vector)
 }
 
 ####################################################################################################################################
 
-set.seed(2021)
-
 # Create empty vectors of exit velocities 
-EV_vector1 <- vector()    #for no change in attack angle
-EV_vector2 <- vector()    #for plus one attack angle
-EV_vector3 <- vector()    #for minus one attack angle
+EV_vector <- vector() 
 
-# global variable to monitor change in attack angle 
-change_in_attack <- 0
-
-predicted_LA_adjust_attack <- function(woba_model, LA_model, player_data, orig_woba, orig_attack, attack, change_in_attack){
+predicted_woba_function<- function(woba_model, linear_model, batter_data){
+  
   # Model the predicted angles given the original attack angle
-  pred_angles <- tibble(lm.preds = predict(LA_model, newdata = player_data))
+  pred_angles <- tibble(lm.preds = predict(linear_model, newdata = batter_data))
   
   # Creating an rnorm with the standard deviation of the residuals to create noise
-  pred_angles <- pred_angles %>% mutate(noise = rnorm(n = length(pred_angles$lm.preds), mean = 0, sd = sigma(LA_model)),
+  pred_angles <- pred_angles %>% mutate(noise = rnorm(n = length(pred_angles$lm.preds), mean = 0, sd = sigma(linear_model)),
                                         launch_angle = lm.preds + noise)
   
+  # Clean up outside angles that are unrealistic 
+  predicted_angles(pred_angles, batter_data)
+    
   # Use sampling to find a corresponding exit velocity for predicted launch angles 
-  EV_vector1 <- sampling(pred_angles, player_data, EV_vector1, orig_attack)
+  EV_vector <- sampling(batter_data, EV_vector)
   
   # Combine  predicted launch angles with predicted exit velocities into one tibble
-  modeled_data <- tibble(launch_angle = pred_angles$launch_angle, launch_speed = EV_vector1)
+  modeled_data <- tibble(launch_angle = pred_angles$launch_angle, launch_speed = EV_vector)
   
   # Predicted woba values from predicted launch angles and exit velocities
-  preds1 <- tibble(gam.preds = predict(woba_model, newdata = modeled_data))  
+  preds <- tibble(gam.preds = predict(woba_model, newdata = modeled_data))  
   
-  # Find the mean expected woba value for the batter 
-  xwOBA1 <- mean(preds1$gam.preds, na.rm = TRUE)
+  # Return expected wobas 
+  return (preds)
   
-  ##################################################
-  # Repeat with a one degree increase in attack angle
-  plus_one_attack <- player_data
-  plus_one_attack$attack_angle <- plus_one_attack$attack_angle + 1
-  
-  # Model the predicted launch angles given the new increased attack angle
-  pred_angles2 <- tibble(lm.preds = predict(LA_model, newdata = plus_one_attack))
-  
-  # Creating an rnorm with the standard deviation of the residuals to create noise
-  pred_angles2 <- pred_angles2 %>% mutate(noise = rnorm(n = length(pred_angles2$lm.preds), mean = 0, sd = sigma(LA_model)),
-                                          launch_angle = lm.preds + noise)
-  
-  # Use sampling to find a corresponding exit velocity for predicted launch angles 
-  EV_vector2 <- sampling(pred_angles2, player_data, EV_vector2, orig_attack)
-  
-  # Combine  predicted launch angles with predicted exit velocities into one tibble
-  modeled_data_plus_one <- tibble(launch_angle = pred_angles2$launch_angle, launch_speed = EV_vector2)
-  
-  # Predicted woba values from predicted launch angles and exit velocities
-  preds2 <- tibble(gam.preds = predict(woba_model, newdata = modeled_data_plus_one))  
-  
-  # Find the mean expected woba value for the batter 
-  xwOBA2 <- mean(preds2$gam.preds, na.rm = TRUE)
-  
-  ##################################################
-  # Repeat with a -1 attack angle
-  minus_one_attack <- player_data
-  minus_one_attack$attack_angle <- minus_one_attack$attack_angle - 1
-  
-  # Model the predicted angles given the new decreased attack angle
-  pred_angles3 <- tibble(lm.preds = predict(LA_model, newdata = minus_one_attack))
-  
-  # Creating an rnorm with the standard deviation of the residuals to create noise
-  pred_angles3 <- pred_angles3 %>% mutate(noise = rnorm(n = length(pred_angles3$lm.preds), 
-                                                        mean = 0, sd = sigma(LA_model)),launch_angle = lm.preds + noise)
-  
-  # Use sampling to find a corresponding exit velocity for predicted launch angles
-  EV_vector3 <- sampling(pred_angles3, player_data, EV_vector3, orig_attack)
-  
-  # Combine  predicted launch angles with predicted exit velocities into one tibble
-  modeled_data_minus_one <- tibble(launch_angle = pred_angles3$launch_angle, launch_speed = EV_vector3)
-  
-  # Predicted woba values from predicted launch angles and exit velocities
-  preds3 <- tibble(gam.preds = predict(woba_model, newdata = modeled_data_minus_one)) 
-  
-  # Find the mean expected woba value for the batter 
-  xwOBA3 <- mean(preds3$gam.preds, na.rm = TRUE)
-  
-  ##################################################
-  
-  # Comparing the different expected woba values 
-  # If original < +1 and the +! woba is greater than the -1 woba
-  if(xwOBA1 < xwOBA2 & xwOBA2 > xwOBA3){
-    # Recursively call with +1 attack angle data
-    change_in_attack <- change_in_attack + 1
-    predicted_LA_adjust_attack(woba_model, LA_model, plus_one_attack, orig_woba, orig_attack, attack+1, change_in_attack)
-  }
-  # Else if original < -1
-  else if (xwOBA1 < xwOBA3){
-    change_in_attack <- change_in_attack - 1
-    # Recursively call with -1 attack angle data
-    predicted_LA_adjust_attack(woba_model, LA_model, minus_one_attack, orig_woba, orig_attack, attack-1, change_in_attack)
-  }
-  # Else
-  else{
-    change_in_attack <- change_in_attack
-    # Return the orig_woba, xwOBA, orig_attack, and attack angles
-    return (tibble(original_woba = orig_woba, predicted_woba = xwOBA1, original_attack = orig_attack, 
-                   reccomended_attack = attack, change_in_attack = change_in_attack)[1,])
-  }
 }
 
 ####################################################################################################################################
 
-# Getting predicted woba values 
-batter_data <- batter_data %>%
-  filter(!is.na(plate_z), !is.na(launch_angle), !is.na(launch_speed)) %>%
+# Cleaning the data
+batter_data <- kemp_data %>%
+  filter(!is.na(plate_z), !is.na(launch_angle), !is.na(launch_speed), !is.na(attack_angle)) %>%
   clean_edges()
 
-batter_woba <- mean(batter_data$woba_value, na.rm = TRUE)
+# Finding predicted woba values for each in play ball, make it a column in dataset
+predicted_values <- predicted_woba_function(woba_model, linear_model, batter_data)
 
-predicted_values <- as.data.frame(predicted_LA_adjust_attack(woba_model, linear_model, batter_data, batter_woba, batter_data$attack_angle, 
-                           batter_data$attack_angle, 0))
+batter_data <- batter_data %>%
+  mutate(pred_woba = predicted_values)
 
-predicted_woba <- predicted_values$predicted_woba
+# Looking at distribution of these woba values
+# Result for heyward: mostly pulled to the left between 0 and 0.5 
+# Result for trout: mostly pulled to the left between 0 and 0.25 
+woba_predictions <- predicted_values %>%
+  ggplot(aes(x = gam.preds)) +
+  geom_density() + 
+  theme_bw()
+
+woba_predictions_box_plot <- predicted_values %>%
+  ggplot(aes(x = gam.preds)) +
+  geom_boxplot() +
+  theme_bw()
+
 
 ####################################################################################################################################
+
+# Let us try and group the predicted woba values into single, double, triple, home runs 
+
+# singles = 0.888
+# doubles = 1.271
+# triples = 1.616
+# home runs = 2.101
+
+woba_values <- batter_data %>%
+  filter(!is.na(events)) %>%
+  group_by(woba_value, events) %>%
+  count()
+
+quantile(predicted_values$gam.preds, probs = c(0.74, 0.76, 0.78, 0.80, 0.82, 0.84, 0.86, 0.88, 0.90, 0.92, 0.94, 0.96, 0.98, 1))
+
+# 86th percentile is 0.8767641 (so 84th-88th is single) (0.86-0.91)
+
+# 92nd percentile is 1.2753117 (so 88th-92nd is double) (0.87-1.3)
+
+# 95th percentile is 1.7594451 (so 92nd-96th is triple) (1.3-1.9)
+
+# 99th percentile is 1.9924783 (so 96th-100th is hr) (1.9-2.1)
+
+# Categorizing the hits by percentiles listed above 
+batter_data <- batter_data %>%
+  mutate(pred_outcome = case_when(
+    pred_woba < quantile(predicted_values$gam.preds, probs = c(0.84)) ~ "Out",
+    pred_woba >= quantile(predicted_values$gam.preds, probs = c(0.84)) & 
+      pred_woba < quantile(predicted_values$gam.preds, probs = c(0.88)) ~ "single",
+    pred_woba >= quantile(predicted_values$gam.preds, probs = c(0.88)) &
+      pred_woba < quantile(predicted_values$gam.preds, probs = c(0.92)) ~ "double",
+    pred_woba >= quantile(predicted_values$gam.preds, probs = c(0.92)) &
+      pred_woba < quantile(predicted_values$gam.preds, probs = c(0.96)) ~ "triple",
+    pred_woba >= quantile(predicted_values$gam.preds, probs = c(0.96)) ~ "home run"
+  )) 
+
+# Find number of each events
+num_events <- batter_data %>%
+  group_by(pred_outcome) %>%
+  summarise(num = n())
+
+# 56 doubles, 56 home runs, 1170 outs, 55 singles , 56 triples
+eq_num <- (0.888 * 55) + (1.271 * 112) + (1.616 * 56) + (2.101 * 56)
+eq_den <- (56 + 56 + 1170 + 55 + 56)
+woba_calc <- eq_num / eq_den
+
+trout_woba <- mean(batter_data$woba_value)
+  
+
+
 
 
 
